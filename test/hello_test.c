39961eb776199c71634c3fd8cdc2f68598a125e4
AwsomeStar123456 <semrahodobasic@icloud.com>
2024-09-18 13:43:28 -0600

    Created our automated tests

--- test/hello_test.c
+++ test/hello_test.c
@@ -10,19 +10,47 @@ void setUp(void) {}
 
 void tearDown(void) {}
 
-void test_capitalization () {
+void test_capitalization() {
+
+    //Test for de-capitalization at end bounds and a few in between.
+    TEST_ASSERT_EQUAL_CHAR(invert_capitalization('A'), 'a');
+    TEST_ASSERT_EQUAL_CHAR(invert_capitalization('Z'), 'z');
+    TEST_ASSERT_EQUAL_CHAR(invert_capitalization('S'), 's');
+    TEST_ASSERT_EQUAL_CHAR(invert_capitalization('M'), 'm');
+    TEST_ASSERT_EQUAL_CHAR(invert_capitalization('J'), 'j');
+
+    //Test for capitalization at end bounds and a few in between.
     TEST_ASSERT_EQUAL_CHAR(invert_capitalization('a'), 'A');
-    TEST_ASSERT_EQUAL_CHAR(invert_capitalization('B'), 'b');
-    TEST_ASSERT_EQUAL_CHAR(invert_capitalization('1'), '1');
+    TEST_ASSERT_EQUAL_CHAR(invert_capitalization('z'), 'Z');
+    TEST_ASSERT_EQUAL_CHAR(invert_capitalization('s'), 'S');
+    TEST_ASSERT_EQUAL_CHAR(invert_capitalization('m'), 'M');
+    TEST_ASSERT_EQUAL_CHAR(invert_capitalization('j'), 'J');
+
+    //Test a few non-alphabet characters. Testing some near the bounds of letters.
+    TEST_ASSERT_EQUAL_CHAR(invert_capitalization('@'), '@');
+    TEST_ASSERT_EQUAL_CHAR(invert_capitalization('['), '[');
+    TEST_ASSERT_EQUAL_CHAR(invert_capitalization('\''), '\'');
+    TEST_ASSERT_EQUAL_CHAR(invert_capitalization('{'), '{');
+    TEST_ASSERT_EQUAL_CHAR(invert_capitalization('?'), '?');
+    TEST_ASSERT_EQUAL_CHAR(invert_capitalization(':'), ':');
+    TEST_ASSERT_EQUAL_CHAR(invert_capitalization('+'), '+');
 }
 
 void test_blink () {
-    TEST_ASSERT_FALSE(blink(2));
-    TEST_ASSERT_FALSE(blink(2));
-    TEST_ASSERT_TRUE(blink(2));
-    TEST_ASSERT_FALSE(blink(2));
-    TEST_ASSERT_TRUE(blink(2));
-    TEST_ASSERT_FALSE(blink(2));
+    int result = 0;
+    int result_prev = 0;
+
+    for(int i = 0 ; i < 100; i++){
+        result = blink(11);
+        
+        if(i % (11*2+1)) {
+            TEST_ASSERT_EQUAL_INT_MESSAGE(result, result_prev, "Blink should not change output.");
+        } else {
+            TEST_ASSERT_NOT_EQUAL_INT_MESSAGE(result, result_prev, "Blink should change output.");
+        }
+        
+        result_prev = result;
+    }
 }
 
 int main (void)
