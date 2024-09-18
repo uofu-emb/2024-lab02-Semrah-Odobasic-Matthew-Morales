eb30359f923aec92bdb0660242e8ece1e9d6f57c
Matthew Morales <mtthwmrls9@gmail.com>
2024-09-18 12:38:04 -0600

    refactoring for testing

--- src/hello_freertos.c
+++ src/hello_freertos.c
@@ -13,22 +13,42 @@
 #include "pico/multicore.h"
 #include "pico/cyw43_arch.h"
 
-int count = 0;
-bool on = false;
-
 #define MAIN_TASK_PRIORITY      ( tskIDLE_PRIORITY + 1UL )
 #define BLINK_TASK_PRIORITY     ( tskIDLE_PRIORITY + 2UL )
 #define MAIN_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 #define BLINK_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 
+void blink (int n);
+char invert_capitalization (char c);
+
+void main_task(__unused void *params);
+void blink_task(__unused void *params);
+
+// Inverts the gpio every call, for a 50% duty cycle. Skips the nth blink
+bool blink (int n) {
+    static int count = 0;
+    static bool on = false;
+
+    if (count++ % (n*2+1)) on = !on;
+    cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
+
+    return on;
+}
+
 // This code runs in another thread to blink an LED
 void blink_task(__unused void *params) {
     hard_assert(cyw43_arch_init() == PICO_OK);
     while (true) {
-        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, on);
-        if (count++ % 11) on = !on;
+        blink(5);
         vTaskDelay(500);
     }
+}
+
+
+char invert_capitalization (char c) {
+    if (c <= 'z' && c >= 'a') return c - 32;
+    else if (c >= 'A' && c <= 'Z') return c + 32;
+    else return c;
 }
 
 // Inverts capitalization of letters passed through UART over USB or leaves them alone if they are not letters.
@@ -36,16 +56,11 @@ void blink_task(__unused void *params) {
 void main_task(__unused void *params) {
     xTaskCreate(blink_task, "BlinkThread",
             BLINK_TASK_STACK_SIZE, NULL, BLINK_TASK_PRIORITY, NULL);
+
     char c;
     while(c = getchar()) {
         putchar(invert_capitalization(c));
     }
-}
-
-char invert_capitalization (char c) {
-    if (c <= 'z' && c >= 'a') return c - 32;
-    else if (c >= 'A' && c <= 'Z') return c + 32;
-    else return c;
 }
 
 // Spools up a main thread which runs the main class, and starts the scheduler
